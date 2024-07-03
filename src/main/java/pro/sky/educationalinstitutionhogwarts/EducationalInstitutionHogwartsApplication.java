5e6beda5e70c8ef386e5d0851c9057ecda6b161b
original <git@github.com:denis-poroshin/variables.git>
2024-07-03 18:43:54 +0300

    Начало

---
+++ src/main/java/pro/sky/educationalinstitutionhogwarts/EducationalInstitutionHogwartsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.educationalinstitutionhogwarts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationalInstitutionHogwartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationalInstitutionHogwartsApplication.class, args);
+	}
+
+}
